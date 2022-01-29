0081985884914a464d52f001292fd38b3ba567ce
adi-alt <77370595+adi-alt@users.noreply.github.com>
2022-01-30 01:33:25 +0530

    hover-update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let first = document.querySelector(".specifica");
 let second = document.querySelector(".specificb");
 let third = document.querySelector(".specificc");
 let fourth = document.querySelector(".specificd");
+let btn = document.querySelector("#btn1");
+let bn = document.querySelector("#btn2");
 first.addEventListener("click", () => {
   first.style.borderBottom = "3px solid blanchedalmond";
   second.style.borderBottom = "";
@@ -25,6 +27,18 @@ third.addEventListener("click", () => {
 fourth.addEventListener("click", () => {
   fourth.style.borderBottom = "3px solid transparent";
   third.style.borderBottom ="";
+  first.style.borderBottom = "";
+  second.style.borderBottom = "";
+});
+btn.addEventListener("click", () => {
+  second.style.borderBottom = "3px solid blanchedalmond";
+  first.style.borderBottom = "";
+  third.style.borderBottom = "";
+  fourth.style.borderBottom ="";
+});
+bn.addEventListener("click", () => {
+  third.style.borderBottom = "3px solid blanchedalmond";
+  fourth.style.borderBottom = "";
   first.style.borderBottom = "";
   second.style.borderBottom = "";
 });
